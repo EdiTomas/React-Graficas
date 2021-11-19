f4682ce45421808c6beafc45ba102750a1af17b3
Edi Tomas <tomas3320141@gmail.com>
2021-11-19 04:09:21 -0600

    Add File

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import {Fragment} from "react"
+import './App.css';
+import FirstGraph from './Componentes/FirstGraph';
+import Login from './Componentes/Login';
+
+function App() {
+  return (<Login/> );
+}
+
+export default App;
