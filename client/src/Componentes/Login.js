75ee48323aec46cca8a2651555db2146d5e79938
Edi Tomas <tomas3320141@gmail.com>
2021-11-20 02:40:46 -0600

    Updated fiel
    it added more function

--- client/src/Componentes/Login.js
+++ client/src/Componentes/Login.js
@@ -1,11 +1,41 @@
 //import  Chart from "react-apexcharts"
-import React, { Component,Fragment } from "react";
+import React, { Component,Fragment,useState } from "react";
+
 
 class Login extends Component{
-      constructor(props){
-          super(props)
+  
+   constructor(props){
+         super(props)
+         this.state ={
+              user:{
+                  nombre:'',
+                  pass:''
+               } 
+            };
+         this.handleChange = this.handleChange.bind(this);
+         this.Login = this.Login.bind(this);
       }
+      handleChange(event) 
+      {  const { name, value } = event.target;
+         const { user } = this.state;
+         this.setState({
+             user: {...user,[name]: value}
+         });
+      }    
+
+      
+      Login(){
+          const usuario = this.state.user.nombre;
+          var contraseña = this.state.user.pass
+             
+          console.log('Nombre:',this.state.user.nombre);  
+          console.log('password:',this.state.user.pass);
+            
+      }
+
+        
    render() {
+      // const {nombre, pass}= this.state;  
     return(<Fragment>
                  <div className ="row" >
                     <div className="card-header" style={{ marginLeft: "400px", marginRight: "400px", background:"#0f2537", fontSize: 30, textAlign: "center", color:"white" }}>
@@ -15,18 +45,17 @@ class Login extends Component{
                        <div className ="form-group"  >
                              <label style={{ color:"white" }}  >Email</label>
                              <div>
-                             <input type="text" id="user" name="user"/>
+                             <input type="text" name="nombre" value={this.state.user.nombre} onChange={this.handleChange} />
                             </div>
                        </div>
                        <div className ="form-group" >
                              <label style={{ marginLeft: "0px",color:"white" }} >Password</label>
                              <div>
-                             <input type="password" id="password" name="password"/>
+                             <input type="password" name="pass" value={this.state.user.pass} onChange={this.handleChange}  />
                              </div>
                         </div>
                         <div >
-                             <button type="button" class="btn btn-dark">Login</button>
-                             <button type="button" class="btn btn-dark">Cancel</button>
+                             <button onClick={this.Login} >Login   </button>
                         </div>
                         
                     </div>   
